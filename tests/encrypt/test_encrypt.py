a68b6c5763cd23ff663b40b13bf103af3d1e6f19
Thiago Guarino <thiago.guarino@outlook.com>
2023-02-27 00:46:14 -0300

    test: test_encrypt_message - task 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,34 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
+# task 2
 def test_encrypt_message():
-    pass
+    correct_mock = {
+        "1": ["mensagem", 3],
+        "2": ["mensagem", 2],
+        "3": ["mensagem", -1]
+    }
+
+    error_mock = [
+        {"input": ["mensagem", "3"], "output": "tipo inválido para key"},
+        {"input": [123456, 3], "output": "tipo inválido para message"},
+    ]
+
+    with pytest.raises(TypeError, match=error_mock[0]["output"]):
+        encrypt_message("mensagem", "3")
+
+    with pytest.raises(TypeError, match=error_mock[1]["output"]):
+        encrypt_message(123456, 3)
+
+    assert encrypt_message(
+        correct_mock["1"][0], correct_mock["1"][1]
+    ) == "nem_megas"
+
+    assert encrypt_message(
+        correct_mock["2"][0], correct_mock["2"][1]
+    ) == "megasn_em"
+
+    assert encrypt_message(
+        correct_mock["3"][0], correct_mock["3"][1]
+    ) == "megasnem"
