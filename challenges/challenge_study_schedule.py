3063cc5b12cda2119fc2a65ddc30d9f035974a13
Thiago Guarino <thiago.guarino@outlook.com>
2023-02-26 22:45:41 -0300

    feat: study_schedule function - task 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,17 @@
 
 # task 1
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    returns the count of students logged in the platform given a target time.
+    """
+    try:
+        n = len(permanence_period)
+        student_counter = 0
+        for index in range(0, n):
+            login_time, logout_time = permanence_period[index]
+            if login_time <= target_time <= logout_time:
+                student_counter += 1
+    except (TypeError):
+        return None
+
+    return student_counter
