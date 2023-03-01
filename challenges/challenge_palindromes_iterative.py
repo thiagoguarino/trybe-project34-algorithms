90e8eebd3fd52abd1e45030295e9b61fbfd06579
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-01 00:03:37 -0300

    feat: function is_palindrome_iterative - task 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
+# task 6
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    this function checks if a string (WORD) is a palindrome or not
+    """
+    normal_word = word.lower()
+    reversed_word = word[::-1].lower()
+
+    if (normal_word == ''): return False 
+
+    for letter in range(0, len(word)):
+        if normal_word[letter] != reversed_word[letter]:
+            return False
+    return True
