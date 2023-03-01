77389c33b3daac4edc247137b99a2754efc0a495
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-01 00:07:22 -0300

    refactor: function is_palindrome_iterative - task 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -6,7 +6,8 @@ def is_palindrome_iterative(word):
     normal_word = word.lower()
     reversed_word = word[::-1].lower()
 
-    if (normal_word == ''): return False 
+    if (normal_word == ''): 
+        return False
 
     for letter in range(0, len(word)):
         if normal_word[letter] != reversed_word[letter]:
